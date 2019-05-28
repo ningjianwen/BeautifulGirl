bb676b9a7197482a7c11299f499d8b602b740e85
ningjianwen <ningjianwen@yeah.net>
2019-05-28 22:20:34 +0800

    Initial Commit

---
+++ BeautifulGirl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeautifulGirl
+//
+//  Created by jianwen ning on 28/05/2019.
+//  Copyright © 2019 jianwen ning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
